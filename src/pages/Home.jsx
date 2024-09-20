899ed14f10b37e7d1dafb42c00278b6c940fe207
Anthea Merton <47039803+Antheam@users.noreply.github.com>
2024-09-20 15:35:25 +0100

    Add popular component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+import React from "react";
+
+export default function home() {
+  return (
+    <div>
+      <Veggie />
+      <Popular />
+    </div>
+  );
+}
